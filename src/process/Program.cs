cf64a8924cf7fff19aa11a2e5e3578af0dd649ca
bmatesevac-tbct <72982744+bmatesevac-tbct@users.noreply.github.com>
2021-01-07 11:57:53 -0500

    Passing an Env variable to a process

---
+++ src/process/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace process
+{
+
+
+
+
+    class Program
+    {
+
+
+        void Run()
+        {
+            using (var proc = new Process())
+            {
+                try
+                {
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    var folder = Directory.GetCurrentDirectory();
+                    var file = "powershell-script.ps1";
+                    var filePath = $"{folder}/{file}";
+                    proc.StartInfo.Environment["EXPECTED-ENV_VAR"] = "EXPECTED-ENV_VAR-VALUE";
+                    proc.StartInfo.FileName = "powershell.exe";
+                    proc.StartInfo.Arguments = filePath;
+                    proc.Start();
+                    string s = proc.StandardOutput.ReadToEnd();
+                    proc.WaitForExit();
+                    Debug.WriteLine(s);
+                    Console.WriteLine(s);
+
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e);
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+        }
+    }
+}
---
+++ src/process/powershell-script.ps1
@@ -0,0 +1,10 @@
+﻿
+
+
+
+Write-Output "Checking for EXPECTED-ENV-VAR"
+# An env variable EXPECTED-ENV-VAR is passed in from the program
+# It should show in this output
+$output = Get-Item Env:EXPECTED*
+Write-Output $output
+Write-Output "Checking for EXPECTED-ENV-VAR complete."
