47f944a96c193679e61adc3cca948f1db1025f97
Bob Matesevac <bob.matesevac@terumobct.com>
2022-02-01 06:39:20 -0500

    Implement PowershellProcess class

---
+++ src/CallPowershellUsingProcess/PowershellProcess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallPowershellUsingProcess
+{
+    public class PowershellProcess
+    {
+        private TimeSpan _timeout = TimeSpan.FromSeconds(20);
+
+        public PowershellProcess()
+        {
+        }
+
+        public void Execute(string command)
+        {
+            Execute(command, _timeout);
+        }
+
+        public void Execute(string command, TimeSpan timeout)
+        {
+            using (var proc = new Process())
+            {
+                proc.StartInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    FileName = "powershell.exe",
+                    Arguments = command,
+                };
+
+                proc.OutputDataReceived += (s, ev) =>
+                {
+                    Console.WriteLine(ev.Data);
+                };
+                proc.ErrorDataReceived += (s, err) =>
+                {
+                    Console.WriteLine(err.Data);
+                };
+                proc.EnableRaisingEvents = true;
+
+                if (!proc.Start())
+                {
+                    throw new Exception($"Failed to execute command ({command})");
+                }
+
+                proc.BeginErrorReadLine();
+                proc.BeginOutputReadLine();
+                proc.WaitForExit((int)timeout.TotalMilliseconds);
+
+                // check return code
+                if (proc.ExitCode != 0)
+                {
+                    throw new Exception($"Command ({command}) exited with code {proc.ExitCode}");
+                }
+
+            }
+        }
+
+
+    }
+
+}
---
+++ src/CallPowershellUsingProcess/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using CallPowershellUsingProcess;
+
+Console.WriteLine("Hello, World!");
+
+var ps = new PowershellProcess();
+
+ps.Execute("ls");
+
